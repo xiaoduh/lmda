3c5bd9c94f433ce7394283872ca9b5ea44a0b6aa
ClémentL <clement.lidar@gmail.com>
2023-09-25 22:13:35 +0200

    css

--- app/404/page.jsx
+++ app/404/page.jsx
@@ -1,6 +1,6 @@
 import FooterApp from "@/components/navigation/FooterApp";
 import React from "react";
-import "../../styles/";
+import "../../styles/index.scss";
 import SectionWrapperHeader from "@/components/section/SectionWrapperHeader";
 
 const notFound = () => {
