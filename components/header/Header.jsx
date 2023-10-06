1f69f6e0d98349500c3e7cd47938c21273c88754
ClémentL <clement.lidar@gmail.com>
2023-10-06 22:29:04 +0200

    &apos;

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -168,11 +168,9 @@ export default async function Header() {
       <Label content={txtLabel} />
       <h1>
         Nous connectons,<br></br>
-        <span className="important">Experts du C++</span> avec les
-        <span className="important">
-          Entreprises de l`&apos;`écosystème C++
-        </span>
-        ,<br></br> en toute <span className="important">transparence</span>.
+        <span className="important">Experts du C++</span> avec les{" "}
+        <span className="important">Entreprises de l&apos;écosystème C++</span>,
+        <br></br> en toute <span className="important">transparence</span>.
       </h1>
       <p>
         Nous croyons au management horizontal et en la transparence.<br></br>
