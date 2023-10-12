62333ac2b3a41ef1be19a4e4cc8954c63056587b
xiaoduh <83875368+xiaoduh@users.noreply.github.com>
2023-10-12 22:27:36 +0200

    Update page.jsx

--- app/partenaire/page.jsx
+++ app/partenaire/page.jsx
@@ -14,8 +14,8 @@ export default async function page() {
       <SectionWrapperHeader id={"form"}>
         <Label content="Connectez-vous à notre réseau de développeurs C++" />
         <ContentSection
-          title={`Bénéficiez d'une visibilité auprès de + ${resProfils.data.length} développeurs C++`}
-          content="Diminuez votre temps de recherche et augmentez la pertinence de vos candidatures grâce à notre spécialisation. Utilisez notre plateforme pour diffuser votre mission au plus large réseau de développeurs C++. Lambda rassemble autour du monde C++ et de ses applications technologiques des milliers de développeurs C++. Discutons de votre recherche et diffusons-la à des milliers de développeurs en veille professionnelle."
+          title={`+ ${resProfils.data.length} développeurs C++ attendent votre projet`}
+          content="Diminuez votre temps de recherche et augmentez la pertinence de vos candidatures grâce à notre spécialisation. Bénéficiez d'une visibilité auprès du plus large réseau de compétences C++."
         />
       </SectionWrapperHeader>
       <Form />
