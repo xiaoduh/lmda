1fa147b697673a9ad28f693f8f3f17f2ecdd9da4
ClémentL <clement.lidar@gmail.com>
2023-10-21 23:05:08 +0200

    contact profil useeffect and usestate

--- app/contact/[id]/page.jsx
+++ app/contact/[id]/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import FooterApp from "@/components/navigation/FooterApp";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../../styles/index.scss";
 import SectionWrapperHeader from "@/components/section/SectionWrapperHeader";
 import FormContactProfil from "@/components/form/FormContactProfil";
@@ -11,21 +11,39 @@ import axios from "axios";
 import { useParams } from "next/navigation";
 
 export default function ContactProfil() {
+  const [isLoaded, setisLoaded] = useState(false);
+  const [data, setData] = useState(null);
   const param = useParams();
-  console.log(param);
-  const data = axios.get(
-    `https://unwavering-friendship-fd7ae40c66.strapiapp.com/api/profils?filters[profil_id][$eq]=${param.id}&populate=*`
-  );
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const data = await axios.get(
+        `https://unwavering-friendship-fd7ae40c66.strapiapp.com/api/profils?filters[profil_id][$eq]=${param.id}&populate=*`
+      );
+      if (data.data.data[0]) {
+        setData(data.data.data[0]);
+        setisLoaded(true);
+      }
+    };
+
+    fetchData().catch(console.error);
+  }, []);
 
   return (
     <main>
       <SectionWrapperHeader>
         <Label content="Prendre contact avec nos experts" />
-        <ContentSection
-          title={`Vous êtes intéressé par ${data.data[0].attributes.first_name} ?`}
-          content="Completez le formulaire de prise de contact ci dessous."
-        />
-        <HeaderProfil data={data.data[0].attributes} />
+        {isLoaded ? (
+          <>
+            <ContentSection
+              title={`Vous êtes intéressé par ${data.attributes.first_name} ?`}
+              content="Completez le formulaire de prise de contact ci dessous."
+            />
+            <HeaderProfil data={data.attributes} />
+          </>
+        ) : (
+          "Chargement des données"
+        )}
       </SectionWrapperHeader>
       <FormContactProfil id={param.id} />
       <FooterApp />
