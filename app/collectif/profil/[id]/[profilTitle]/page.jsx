9cf89025cc1a884583296b1bb2eb78150c995ee5
ClémentL <clement.lidar@gmail.com>
2023-10-21 23:16:11 +0200

    ueseffect for fecthing data

--- app/collectif/profil/[id]/[profilTitle]/page.jsx
+++ app/collectif/profil/[id]/[profilTitle]/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import FooterApp from "@/components/navigation/FooterApp";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../../../../styles/index.scss";
 import HeaderProfil from "@/components/profil/HeaderProfil";
 import SectionWrapperProfil from "@/components/section/SectionWrapperProfil";
@@ -14,38 +14,55 @@ import axios from "axios";
 import { useParams } from "next/navigation";
 
 export default function Profil() {
+  const [isLoaded, setisLoaded] = useState(false);
+  const [data, setData] = useState(null);
   const param = useParams();
-  console.log(param);
-  const data = axios.get(
-    `https://unwavering-friendship-fd7ae40c66.strapiapp.com/api/profils?filters[profil_id][$eq]=${param.id}&populate=*`
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get(
+        `https://unwavering-friendship-fd7ae40c66.strapiapp.com/api/profils?filters[profil_id][$eq]=${param.id}&populate=*`
+      );
+      if (data.data.data[0]) {
+        setData(data.data.data[0]);
+        setisLoaded(true);
+      }
+    };
+
+    fetchData().catch(console.error);
+  }, []);
 
   return (
     <main>
       <SectionWrapperProfil>
-        <HeaderProfil data={data.data.data[0].attributes} />
-        <ContentContainer>
-          <SkillsContainer>
-            <h2 className="title-section">Compétences & Intérêts</h2>
-            <Skill
-              title={"Compétences logicielles"}
-              data={data.data.data[0].attributes.software_skills.data}
-            />
-            <h2 className="title-section">Avis & recommandations</h2>
-
-            <Reference name="Fonctionnalité à venir 🚧" />
-          </SkillsContainer>
-          <ExperiencesContainer>
-            {data.data.data[0].attributes.experiences.data.map((experience) => {
-              return (
-                <Experience
-                  key={experience.attributes.createdAt}
-                  data={experience}
+        {isLoaded ? (
+          <>
+            <HeaderProfil data={data.attributes} />
+            <ContentContainer>
+              <SkillsContainer>
+                <h2 className="title-section">Compétences & Intérêts</h2>
+                <Skill
+                  title={"Compétences logicielles"}
+                  data={data.attributes.software_skills.data}
                 />
-              );
-            })}
-          </ExperiencesContainer>
-        </ContentContainer>
+                <h2 className="title-section">Avis & recommandations</h2>
+
+                <Reference name="Fonctionnalité à venir 🚧" />
+              </SkillsContainer>
+              <ExperiencesContainer>
+                {data.attributes.experiences.data.map((experience) => {
+                  return (
+                    <Experience
+                      key={experience.attributes.createdAt}
+                      data={experience}
+                    />
+                  );
+                })}
+              </ExperiencesContainer>
+            </ContentContainer>
+          </>
+        ) : (
+          "Chargement des données"
+        )}
       </SectionWrapperProfil>
       <FooterApp />
     </main>
