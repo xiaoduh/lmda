9ff8d99ffe53bf66ecf870c3101dc6feefe837ba
ClémentL <clement.lidar@gmail.com>
2023-10-22 13:02:10 +0200

    async useeffect

--- app/jobs/description/[jobId]/page.jsx
+++ app/jobs/description/[jobId]/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../../../styles/index.scss";
 import FooterApp from "@/components/navigation/FooterApp";
 import HeaderJob from "@/components/job/HeaderJob";
@@ -7,57 +7,72 @@ import DescriptionContainer from "@/components/job/DescriptionContainer";
 import SectionWrapperProfil from "@/components/section/SectionWrapperProfil";
 import ContentContainer from "@/components/profil/ContentContainer";
 import SkillsContainer from "@/components/profil/SkillsContainer";
-import Requirement from "@/components/job/Requirement";
 import JobDescription from "@/components/job/JobDescription";
 import axios from "axios";
 import { useParams } from "next/navigation";
 
 export default function Description() {
+  const [isLoaded, setisLoaded] = useState(false);
+  const [data, setData] = useState(null);
   const param = useParams();
-  console.log(param);
-  const data = axios.get(
-    `https://unwavering-friendship-fd7ae40c66.strapiapp.com/api/jobs?filters[job_id][$eq]=${param.jobId}&populate=*`
-  );
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const data = await axios.get(
+        `https://unwavering-friendship-fd7ae40c66.strapiapp.com/api/jobs?filters[job_id][$eq]=${param.jobId}&populate=*`
+      );
+      if (data.data.data[0]) {
+        setData(data.data.data[0]);
+        setisLoaded(true);
+      }
+    };
+
+    fetchData().catch(console.error);
+  }, []);
 
   return (
     <main>
       <SectionWrapperProfil>
-        <HeaderJob
-          title={data.data.data[0].attributes.title}
-          localisation={data.data.data[0].attributes.localisation}
-          work_organisation={data.data.data[0].attributes.work_organisation}
-          id={param.jobId}
-          date={data.data.data[0].attributes.createdAt}
-        />
-        <ContentContainer>
-          <SkillsContainer>
-            <h2 className="title-section">Profil recherché</h2>
-            <div className="requirement">
-              <h3>Compétences attendues</h3>
-              <ul>
-                {data.data.data[0].attributes.skills_required.requirement.map(
-                  (requirement) => {
-                    return <li key={requirement}>👉 {requirement}</li>;
-                  }
-                )}
-              </ul>
-            </div>
-            <div className="requirement">
-              <h3>Environnement technique</h3>
-              <ul>
-                {data.data.data[0].attributes.technical_stack.stack.map(
-                  (stack) => {
-                    return <li key={stack}>👉 {stack}</li>;
-                  }
-                )}
-              </ul>
-            </div>
-          </SkillsContainer>
-          <DescriptionContainer>
-            <h2 className="title-section">Description</h2>
-            <JobDescription jobData={data.data.data[0].attributes} />
-          </DescriptionContainer>
-        </ContentContainer>
+        {isLoaded ? (
+          <>
+            <HeaderJob
+              title={data.attributes.title}
+              localisation={data.attributes.localisation}
+              work_organisation={data.attributes.work_organisation}
+              id={param.jobId}
+              date={data.attributes.createdAt}
+            />
+            <ContentContainer>
+              <SkillsContainer>
+                <h2 className="title-section">Profil recherché</h2>
+                <div className="requirement">
+                  <h3>Compétences attendues</h3>
+                  <ul>
+                    {data.attributes.skills_required.requirement.map(
+                      (requirement) => {
+                        return <li key={requirement}>👉 {requirement}</li>;
+                      }
+                    )}
+                  </ul>
+                </div>
+                <div className="requirement">
+                  <h3>Environnement technique</h3>
+                  <ul>
+                    {data.attributes.technical_stack.stack.map((stack) => {
+                      return <li key={stack}>👉 {stack}</li>;
+                    })}
+                  </ul>
+                </div>
+              </SkillsContainer>
+              <DescriptionContainer>
+                <h2 className="title-section">Description</h2>
+                <JobDescription jobData={data.attributes} />
+              </DescriptionContainer>
+            </ContentContainer>
+          </>
+        ) : (
+          "Chargement des données"
+        )}
       </SectionWrapperProfil>
       <FooterApp />
     </main>
--- app/jobs/postuler/[jobId]/[jobTitle]/page.jsx
+++ app/jobs/postuler/[jobId]/[jobTitle]/page.jsx
@@ -4,27 +4,45 @@ import FormApply from "@/components/form/FormApply";
 import Label from "@/components/label/Label";
 import SectionWrapper from "@/components/section/SectionWrapper";
 import SectionWrapperHeader from "@/components/section/SectionWrapperHeader";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../../../../styles/index.scss";
 import FooterApp from "@/components/navigation/FooterApp";
 import axios from "axios";
 import { useParams } from "next/navigation";
 
 export default function ApplyJob() {
+  const [isLoaded, setisLoaded] = useState(false);
+  const [data, setData] = useState(null);
   const param = useParams();
-  console.log(param);
-  const data = axios.get(
-    `https://unwavering-friendship-fd7ae40c66.strapiapp.com/api/jobs?filters[job_id][$eq]=${param.jobId}&populate=*`
-  );
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const data = await axios.get(
+        `https://unwavering-friendship-fd7ae40c66.strapiapp.com/api/jobs?filters[job_id][$eq]=${param.jobId}&populate=*`
+      );
+      if (data.data.data[0]) {
+        setData(data.data.data[0]);
+        setisLoaded(true);
+      }
+    };
+
+    fetchData().catch(console.error);
+  }, []);
 
   return (
     <main>
       <SectionWrapperHeader id={"postuler"}>
         {/* <Label content="Lambda est un agrégateur de missions C++" /> */}
-        <ContentSection
-          title={data.data.data[0].attributes.title}
-          content={data.data.data[0].attributes.job_id}
-        />
+        {isLoaded ? (
+          <>
+            <ContentSection
+              title={data.attributes.title}
+              content={data.attributes.job_id}
+            />
+          </>
+        ) : (
+          "Chargement des données"
+        )}
       </SectionWrapperHeader>
       <SectionWrapper>
         <FormApply />
