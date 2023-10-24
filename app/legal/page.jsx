fe5c385f8c51ff9a87f288c16f9e455dde55facf
ClémentL <clement.lidar@gmail.com>
2023-10-24 23:01:20 +0200

    metadata correction

--- app/legal/page.jsx
+++ app/legal/page.jsx
@@ -6,7 +6,7 @@ import SectionWrapperHeader from "@/components/section/SectionWrapperHeader";
 import Link from "next/link";
 
 export const metadata = {
-  title: "Lambda | Mentions légales",
+  title: "Mentions légales |Lambda",
   description:
     "Le présent site, accessible à l'URL www.lambda.fr,est édité par : Lambda-Labs, société au capital de 3000 euros, inscrite au R.C.S.de BOBIGNY sous le numéro 980 507 008, dont le siège social est situé au 2 Esplanade Ferdinand-Magellan 93160 Noisy-le-Grand, représentée par Mohamed-Amine ABIDI dûment habilité.",
 };
