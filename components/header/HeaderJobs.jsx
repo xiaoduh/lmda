fd784848a74e14eafce187fa7b052e8afe27fae3
clement <clement.lidar@gmail.com>
2023-11-02 22:24:48 +0100

    Votre prochaine mission se trouve ici

--- components/header/HeaderJobs.jsx
+++ components/header/HeaderJobs.jsx
@@ -1,11 +1,11 @@
-
 export default async function HeaderJobs() {
   return (
     <header className="main-header" id="header">
-      <h1>Soyez visible auprès des entreprises</h1>
+      <h1>Votre prochaine mission se trouve ici</h1>
       <p>
-        Ne ratez plus aucunes opportunités. Nous vous connectons à l'écosystème
-        C++. Postulez en un clic aux missions qui vous intéressent.
+        Ne ratez plus aucunes opportunités. Nous vous connectons à
+        l&apos;écosystème C++. Postulez en un clic aux missions qui vous
+        intéressent.
       </p>
     </header>
   );
