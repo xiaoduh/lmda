150e1294197881b00d7ac3491cabb124fed55305
clement <clement.lidar@gmail.com>
2023-11-02 22:36:22 +0100

    &apos;

--- components/header/HeaderCollectif.jsx
+++ components/header/HeaderCollectif.jsx
@@ -1,10 +1,10 @@
 export default async function HeaderCollectif() {
   return (
     <header className="main-header" id="header">
-      <h1>Accéder aux meilleurs experts C/C++ n'a jamais été aussi facile.</h1>
+      <h1>Accéder aux meilleurs experts C/C++ n&apos;a jamais été aussi facile.</h1>
       <p>
-        Le plus grand réseau d'experts C/C++ se trouve ici. Recherchez et
-        contactez le spécialiste qu'il vous manque parmis notre réseau.
+        Le plus grand réseau d&apos;experts C/C++ se trouve ici. Recherchez et
+        contactez le spécialiste qu&apos;il vous manque parmis notre réseau.
       </p>
     </header>
   );
