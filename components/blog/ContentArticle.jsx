ab0eb74bdf7d7d5c068e3123584e4cadc61cfe12
ClémentL <clement.lidar@gmail.com>
2023-11-12 00:13:45 +0100

    key props

--- components/blog/ContentArticle.jsx
+++ components/blog/ContentArticle.jsx
@@ -1,18 +1,28 @@
 export default async function ContentArticle({ data }) {
-//   console.log(data[0].attributes.content);
+  //   console.log(data[0].attributes.content);
   return (
     <article className="body-content">
       {data[0].attributes.content.map((content) => {
         if (content.type === "paragraph") {
-          return <p className="p-article">{content.children[0].text}</p>;
+          return (
+            <p className="p-article" key={content.children[0].text}>
+              {content.children[0].text}
+            </p>
+          );
         } else if (content.type === "heading") {
-          return <h2 className="article-h2">{content.children[0].text}</h2>;
+          return (
+            <h2 className="article-h2" key={content.children[0].text}>
+              {content.children[0].text}
+            </h2>
+          );
         } else if (content.type === "list") {
           return (
-            <ul className="ul-article">
+            <ul key={Math.random()} className="ul-article">
               {content.children.map((item) => {
                 return (
-                  <li className="bullet-point">{item.children[0].text}</li>
+                  <li className="bullet-point" key={item.children[0].text}>
+                    {item.children[0].text}
+                  </li>
                 );
               })}
             </ul>
