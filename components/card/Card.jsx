89ad2e42d21a59d7d2cc76118e5dcb2fa926c4f6
ClémentL <clement.lidar@gmail.com>
2023-12-17 23:20:52 +0100

    pictos inside cards

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import React from "react";
 
 const Card = ({ title, content, style, img }) => {
   return (
     <div className={`card`}>
       {img ? (
-        <Image src={img.src} width={24} height={24} alt={img.alt} />
+        <Image src={img.src} width={28} height={28} alt={img.alt} />
       ) : null}
       <h3 id={`${style}`}>{title}</h3>
       <p>{content}</p>
